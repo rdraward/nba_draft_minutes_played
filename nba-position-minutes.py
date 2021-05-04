07fc1e372c97fd538353f48a07060c9e0991e1d7
rdraward <rdraward@gmail.com>
2021-05-03 20:16:11 -0400

    add option to sort data by minutes played

--- nba-position-minutes.py
+++ nba-position-minutes.py
@@ -10,11 +10,12 @@ def q90(x):
     return x.quantile(0.90)
 
 # get user input
-min_year = int(input("Enter start year (ex. 2000):") or "1989")
-max_year = int(input("Enter end year (ex. 2005):") or "2006")
-regression_degree = int(input("Enter regression degree (1=linear, 2=poly):") or "1")
-raw_data_file_name = input("Enter the name of the raw data file to save:")
-group_x_picks = int(input("Enter n number of picks to group:") or False)
+min_year = int(input("Enter start year (ex. 2000): ") or "1989")
+max_year = int(input("Enter end year (ex. 2005): ") or "2006")
+regression_degree = int(input("Enter regression degree (0=none, 1=linear, 2=poly): ") or "0")
+raw_data_file_name = input("Enter the name of the raw data file to save: ")
+group_x_picks = int(input("Enter n number of picks to group: ") or False)
+sort_by_minutes = input("Sort by career minutes played? (T, F): ").lower() == "t"
 
 # query data
 dfs = []
@@ -26,10 +27,14 @@ for year in range(min_year, max_year + 1):
   df['PICK'] = df['PICK'].astype(int)
   df['TOTALS_MP'] = pd.to_numeric(df['TOTALS_MP'], 'coerce').fillna(0).astype(int)
 
+  if sort_by_minutes:
+    df.sort_values(by=['TOTALS_MP'], inplace=True, ascending=False)
+    df.reset_index(inplace=True, drop=True)
+
   dfs.append(df)
 
 # get average career minutes played
-df = pd.concat(dfs).groupby('PICK').agg({'TOTALS_MP': ['mean', q10, q90] })
+df = pd.concat(dfs).groupby(level=0).agg({'TOTALS_MP': ['mean', q10, q90] })
 
 # flatten columns
 df.columns = ['_'.join(x) if x[0] != '' else x[1] for x in df.columns]
@@ -43,23 +48,30 @@ if group_x_picks:
 if raw_data_file_name:
   df.to_csv(r'raw_results/{0}.txt'.format(raw_data_file_name), header=None, index=None, sep=' ', mode='a')
 
+# build plot
+plt.scatter(df.index, df['TOTALS_MP_mean'], label='Avg Career Min Played', s=20)
+plt.fill_between(df.index, df['TOTALS_MP_q10'], df['TOTALS_MP_q90'], interpolate=True, color='lightgrey', alpha=0.5, label='10-90% Confidence')
+plt.legend()
+
 # regression
-d = np.polyfit(df['PICK'], df['TOTALS_MP_mean'], regression_degree)
-f = np.poly1d(d)
-df.insert(2, 'REGRESSION', f(df['PICK']))
+if regression_degree > 0:
+  d = np.polyfit(df.index, df['TOTALS_MP_mean'], regression_degree)
+  f = np.poly1d(d)
+  df.insert(2, 'REGRESSION', f(df.index))
+
+  plt.plot(df.index, df['REGRESSION'], color='red', label="Regression")
+
+# set axis limits, labels
+ax = plt.gca()
+ax.set_xlim(left=0, right=60)
+ax.set_xlabel('Pick #')
+ax.set_ylabel('Total Career Minutes Played')
 
 # chart title
 regression_title = 'polynomial' if regression_degree > 1 else 'linear'
 title = '{0} - {1}: {2} regression with confidence band'.format(min_year, max_year, regression_title)
-
-# build plot
-ax = df.plot(x='PICK', y='TOTALS_MP_mean', title=title, kind='scatter')
-df.plot(x='PICK', y='REGRESSION', color='red', ax=ax, label="Regression")
-plt.fill_between(df['PICK'], df['TOTALS_MP_q10'], df['TOTALS_MP_q90'], interpolate=True, color='lightgrey', alpha=0.5, label='10-90% Confidence')
-plt.legend()
-
-ax.set_xlim(left=0, right=60)
-ax.set_xlabel('Pick #')
-ax.set_ylabel('Total Career Minutes Played')
+if sort_by_minutes:
+  title += '\nsorted by minutes played'
+plt.title(title)
 
 plt.show()
