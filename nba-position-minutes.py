59e7b9a59ca1adcfc4505b829f6f33089d48406d
rdraward <rdraward@gmail.com>
2021-05-20 00:08:46 -0400

    scrape playoff minutes

--- nba-position-minutes.py
+++ nba-position-minutes.py
@@ -11,12 +11,38 @@ def q90(x):
     return x.quantile(0.90)
 
 def get_playoff_minutes(row):
+  p = row['PLAYER']
+  print('processing: ' + p)
+
   try:
-    player_df = get_stats(row['PLAYER'], stat_type='TOTALS', playoffs=True, career=True)
+    player = p.encode('cp1252').decode()
+  except:
+    print('encoding error ' + p)
+    
+    index = p.find('Ä')
+    while index > -1 and index <= len(p):
+      try:
+        s = 'Ä' + p[index+1]
+        s.encode('cp1252').decode()
+        index += 1
+      except:
+        p = p[:index + 1] + '‡' + p[index + 1:]
+
+      index = p.find('Ä', index)
+
+    player = p.encode('cp1252', 'ignore').decode()
+    print('encoding error fixed: ' + player)
+
+  try:
+    player_df = get_stats(player, stat_type='TOTALS', playoffs=True, career=True, ask_matches=False)
     if not player_df.empty:
       return player_df['MP'].sum()
-  except (ValueError, IndexError, AttributeError):
+  except (ValueError, IndexError):
     return 0
+  except:
+    print('api error ' + player)
+
+  return -1
 
 # get user input
 min_year = int(input("Enter start year (ex. 2000): ") or "1989")
@@ -30,8 +56,9 @@ sort_by_minutes = input("Sort by career minutes played? (T, F): ").lower() == "t
 dfs = []
 for year in range(min_year, max_year + 1):
   df = get_draft_class(year)
-
   df['PLAYOFF_MP'] = df.apply(get_playoff_minutes, axis=1)
+
+  df.to_csv(r'raw_results/draft_year/{0}_raw.csv'.format(year), header=None, index=None, sep=',', mode='a')
 
   # only use relevant columns
   df = df[['PICK','TOTALS_MP', 'PLAYOFF_MP']]
@@ -42,10 +69,12 @@ for year in range(min_year, max_year + 1):
     df.sort_values(by=['TOTALS_MP'], inplace=True, ascending=False)
     df.reset_index(inplace=True, drop=True)
 
+  df.to_csv(r'raw_results/draft_year/{0}.csv'.format(year), header=None, index=None, sep=',', mode='a')
+
   dfs.append(df)
 
 # get average career minutes played
-df = pd.concat(dfs).groupby(level=0).agg({'TOTALS_MP': ['mean', q10, q90] })
+df = pd.concat(dfs).groupby(level=0).agg({'TOTALS_MP': ['mean', q10, q90], 'PLAYOFF_MP': ['mean', q10, q90] })
 
 # flatten columns
 df.columns = ['_'.join(x) if x[0] != '' else x[1] for x in df.columns]
@@ -62,6 +91,8 @@ if raw_data_file_name:
 # build plot
 plt.scatter(df['index'], df['TOTALS_MP_mean'], label='Avg Career Min Played', s=20)
 plt.fill_between(df['index'], df['TOTALS_MP_q10'], df['TOTALS_MP_q90'], interpolate=True, color='lightgrey', alpha=0.5, label='10-90% Confidence')
+plt.scatter(df['index'], df['PLAYOFF_MP_mean'], label='Avg Playoff Min Played', s=20)
+plt.fill_between(df['index'], df['PLAYOFF_MP_q10'], df['PLAYOFF_MP_q90'], interpolate=True, color='lavender', alpha=0.5, label='10-90% Playoff Confidence')
 plt.legend()
 
 # regression
